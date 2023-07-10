771fdf508d38a8ef9f522ee5a8da37f122c3fc01
Rodrigo Magalhães <35574839+rodmag@users.noreply.github.com>
2023-07-10 10:57:33 -0300

    Update CreateSession.java

--- src/main/java/GC/sessions/CreateSession.java
+++ src/main/java/GC/sessions/CreateSession.java
@@ -12,14 +12,12 @@ import java.net.URISyntaxException;
 public class CreateSession {
     public static void main(String[] args) throws URISyntaxException {
 
-        String filePath = "C:\\Users\\w104860\\OneDrive - Worldline\\Documents\\Java\\ConnectJava\\src\\main\\resources\\Caminho";
-        File file = new File(filePath);
-        URI propertiesUrl= file.toURI();
-        Client client = Factory.createClient(propertiesUrl, "dfd1952dc924d813", "3It1Z8siQF+pxfa6TzSo64qMDq8uJ+iLS4TWnS1pCy0=");
+        //Utiliza API e Secret key configurada no arquivo PreparaConexao.
+        Client client = PreparaConexao.startConection();
 
         SessionRequest body = new SessionRequest();
 
-        SessionResponse response = client.merchant("10179").sessions().create(body);
+        SessionResponse response = client.merchant("merchantID").sessions().create(body);
 
         System.out.println("assetUrl : " + response.getAssetUrl());
         System.out.println("clientApiUrl: " + response.getClientApiUrl());
