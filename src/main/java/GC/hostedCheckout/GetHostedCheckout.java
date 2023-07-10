08c30f767f276ffe157f81d7c782c5d1fca334f2
Rodrigo Magalhães <35574839+rodmag@users.noreply.github.com>
2023-07-10 10:54:56 -0300

    Update GetHostedCheckout.java

--- src/main/java/GC/hostedCheckout/GetHostedCheckout.java
+++ src/main/java/GC/hostedCheckout/GetHostedCheckout.java
@@ -10,7 +10,7 @@ public class GetHostedCheckout {
         //Utiliza API e Secret key configurada no arquivo PreparaConexao.
         Client client = PreparaConexao.startConection();
 
-        GetHostedCheckoutResponse response = client.merchant("10179").hostedcheckouts().get("0645e96b-08ba-71ff-90ae-3dad09bf3184");
+        GetHostedCheckoutResponse response = client.merchant("merchantId").hostedcheckouts().get("0645e96b-08ba-71ff-90ae-3dad09bf3184");
 
         System.out.printf("The payment status is: " + response.getStatus());
 
