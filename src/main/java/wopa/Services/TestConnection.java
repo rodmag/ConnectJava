bfba824480b3fc3f1ec0ec930b47d899a89494b0
Rodrigo Magalhães <35574839+rodmag@users.noreply.github.com>
2023-07-10 10:48:52 -0300

    Update TestConnection.java

--- src/main/java/wopa/Services/TestConnection.java
+++ src/main/java/wopa/Services/TestConnection.java
@@ -11,7 +11,7 @@ public class TestConnection {
         //Utiliza API e Secret key configurada no arquivo PreparaConexao.
         Client client = PreparaConexao.startConection();
 
-        com.ingenico.connect.gateway.sdk.java.domain.services.TestConnection response = client.merchant("1908466593").services().testconnection();
+        com.ingenico.connect.gateway.sdk.java.domain.services.TestConnection response = client.merchant("merchantId").services().testconnection();
 
         System.out.println("Resposta do Test Connection: " + response.getResult());
 
